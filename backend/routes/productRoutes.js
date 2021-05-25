0c4b6166f587daff6acea3835f693fbb537d77fb
Yonas Egzeru <yonasegzeru@Yonass-MacBook-Air.local>
2021-05-25 00:15:04 -0400

    fetch data from mogodb

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
+
+const router = express.Router();
+
+//@desc     fetch all products
+//@route    GET api/products
+//@access   public
+
+router.get(
+  '/',
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    res.json(products);
+  })
+);
+
+//@desc     fetch a single product
+//@route    GET api/products/:id
+//@access   public
+
+router.get(
+  '/:id',
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({message: 'Product not found'});
+    }
+  })
+);
+
+export default router;
